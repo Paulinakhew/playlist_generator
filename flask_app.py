add9c58c3ba19b628c608d5ee12927f0ef44e71a
Paulina Khew <paulinakhew12345@hotmail.com>
2021-01-31 15:47:20 -0500

    :truck: Initial commit of migrating get_info.html to profile.html

--- flask_app.py
+++ flask_app.py
@@ -82,24 +82,18 @@ def auth():
     return res
 
 
-@app.route('/callback')
+@app.route("/callback", methods=["GET", "POST"])
 def callback():
-    error = request.args.get('error')
-    state = request.args.get('state')
-
-    if error:
-        app.logger.error('Error: %s, State: %s', error, state)
-        abort(400)
-
-    return render_template('profile.html')
-
-
-@app.route("/get_info", methods=["GET", "POST"])
-def get_info():
     if request.method == "GET":
-        return render_template("get_info.html")
+        error = request.args.get('error')
+        state = request.args.get('state')
+
+        if error:
+            app.logger.error('Error: %s, State: %s', error, state)
+            abort(400)
+
+        return render_template('profile.html')
     elif request.method == "POST":
-        # TODO: add try/except here
         cp = c.CreatePlaylist()
 
         submitted_token = request.form["token"]
@@ -125,9 +119,45 @@ def get_info():
             return render_template("get_info.html", failure=True, info=result)
         elif result and 'snapshot_id' in result:
             return render_template("get_info.html", success=True, info=result)
-        return render_template("get_info.html")
+        return render_template("profile.html")
     else:
-        return render_template("get_info.html")
+        return render_template("profile.html")
+
+
+# @app.route("/get_info", methods=["GET", "POST"])
+# def get_info():
+#     if request.method == "GET":
+#         return render_template("get_info.html")
+#     elif request.method == "POST":
+#         # TODO: add try/except here
+#         cp = c.CreatePlaylist()
+
+#         submitted_token = request.form["token"]
+#         submitted_userid = request.form["userid"]
+#         cp.set_credentials(submitted_userid, submitted_token)
+
+#         submitted_songs = request.form["songs"]
+#         del1 = request.form["del1"]
+#         del2 = request.form["del2"]
+
+#         playlist_name = request.form['playlist_name']
+#         playlist_description = request.form['playlist_description']
+
+#         # artist_song = request.form['artist_song']
+#         if request.form.get('artist_song'):
+#             artist_song = True
+#         else:
+#             artist_song = False
+
+#         result = cp.add_submitted_songs_to_playlist(submitted_songs, del1, del2, playlist_name, playlist_description, artist_song)
+
+#         if result and 'error' in result:
+#             return render_template("get_info.html", failure=True, info=result)
+#         elif result and 'snapshot_id' in result:
+#             return render_template("get_info.html", success=True, info=result)
+#         return render_template("get_info.html")
+#     else:
+#         return render_template("get_info.html")
 
 
 if __name__ == "__main__":
